66736f6aa06d9358040b1beae269e19254c8721c
Emily Eldar <eldaremily@gmail.com>
2024-12-19 11:10:59 -0800

    Adds exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
-    while true {
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
@@ -10,6 +11,16 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+
+        let arguments: Vec<_> = input.trim().split(" ").collect();
+
+        if arguments.len() > 0 {
+            // Cover builtins
+            match arguments[0] {
+                "exit" => process::exit(0),
+                &_ => (),
+            }
+        }
 
         println!("{0}: command not found", input.trim());
     }
