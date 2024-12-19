0ad458b8d1e67a586826c79e978bc530b7c49c11
Emily Eldar <eldaremily@gmail.com>
2024-12-19 11:23:02 -0800

    Adds echo builtin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 use std::io::{self, Write};
 use std::process;
 
+fn echo(arguments: &[&str]) {
+    println!("{0}", arguments.join(" "));
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -12,16 +16,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let arguments: Vec<_> = input.trim().split(" ").collect();
+        let arguments: Vec<_> = input.trim().split(" ").filter(|s| !s.is_empty()).collect();
 
         if arguments.len() > 0 {
             // Cover builtins
             match arguments[0] {
                 "exit" => process::exit(0),
-                &_ => (),
+                "echo" => echo(&arguments[1..]),
+                &_ => println!("{0}: command not found", input.trim()),
             }
         }
-
-        println!("{0}: command not found", input.trim());
     }
 }
