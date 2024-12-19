ae196fd18e566794593fb6dd82d06356d8bb0ae5
Emily Eldar <eldaremily@gmail.com>
2024-12-19 11:32:09 -0800

    Adds type builtin

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@
 use std::io::{self, Write};
 use std::process;
 
+const BUILTINS: [&str; 3] = ["exit", "echo", "type"];
+
 fn echo(arguments: &[&str]) {
     println!("{0}", arguments.join(" "));
+}
+
+fn type_check(arguments: &[&str]) {
+    if arguments.len() != 1 {
+        println!("type: should have only one argument")
+    } else {
+        if BUILTINS.contains(&arguments[0]) {
+            println!("{} is a shell builtin", arguments[0]);
+        } else {
+            println!("{}: not found", arguments [0]);
+        }
+    }
 }
 
 fn main() {
@@ -23,6 +37,7 @@ fn main() {
             match arguments[0] {
                 "exit" => process::exit(0),
                 "echo" => echo(&arguments[1..]),
+                "type" => type_check(&arguments[1..]),
                 &_ => println!("{0}: command not found", input.trim()),
             }
         }
