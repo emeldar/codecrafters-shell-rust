30dbf7695ba6bdee7ab3b61a6cabe68584736699
Emily Eldar <eldaremily@gmail.com>
2025-02-25 00:58:53 -0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -103,13 +103,27 @@ fn cd(cwd: &mut PathBuf, arguments: &[&str]) {
 fn parse_args(input: &str) -> Vec<String> {
     let mut args = vec![];
     let mut single_quote = false;
+    let mut double_quote = false;
     let mut current_arg = String::new();
 
     for c in input.chars() {
         match c {
-            '\'' => single_quote = !single_quote,
-            ' ' => {
+            '\'' => {
+                if double_quote {
+                    current_arg.push(c);
+                } else {
+                    single_quote = !single_quote;
+                }
+            },
+            '\"' => {
                 if single_quote {
+                    current_arg.push(c);
+                } else {
+                    double_quote = !double_quote;
+                }
+            },
+            ' ' => {
+                if single_quote || double_quote {
                     current_arg.push(c);
                 } else {
                     if !current_arg.is_empty() {
